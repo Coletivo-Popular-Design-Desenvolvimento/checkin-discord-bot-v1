1ba9a79511997b799bccdffd5e7da1ebbdd6e134
milenascc-cpdd <milenasccarneiro@outlook.com>
2025-05-12 16:27:58 -0300

    CPDD-152: interface com métodos de find por entidades relacionadas

--- src/domain/interfaces/repositories/IMessageRepository.ts
+++ src/domain/interfaces/repositories/IMessageRepository.ts
@@ -3,11 +3,21 @@ import { MessageEntity } from "../../entities/Message";
 export interface IMessageRepository {
   create(message: Omit<MessageEntity, "id">): Promise<MessageEntity>;
   createMany(messages: Omit<MessageEntity, "id">[]): Promise<number>;
+  
   findById(id: number): Promise<MessageEntity | null>;
+  findByChannelId(
+    channelId: string
+  ): Promise<MessageEntity[] | null>;
   findByDiscordId(
-    id: string
-  ): Promise<MessageEntity | null>;
+    discordId: string
+  ): Promise<MessageEntity[] | null>;
+  findByUserId(
+    userId: number
+  ): Promise<MessageEntity[] | null>;
+  
   listAll(limit?: number): Promise<MessageEntity[]>;
+  
   updateById(id: number, message: Partial<MessageEntity>): Promise<MessageEntity | null>;
+  
   deleteById(id: number): Promise<boolean>;
 }
