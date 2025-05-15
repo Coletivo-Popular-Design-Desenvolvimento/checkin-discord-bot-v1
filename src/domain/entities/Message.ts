acc506878a6fca9198f9ad20daa4893579b253af
milenascc-cpdd <milenasccarneiro@outlook.com>
2025-05-15 18:30:23 -0300

    CPDD-152: coluna isDeleted na classe de entidade

--- src/domain/entities/Message.ts
+++ src/domain/entities/Message.ts
@@ -3,6 +3,7 @@ export class MessageEntity {
       public readonly channelId: number,
       public readonly discordId: string,
       public readonly discordCreatedAt: Date,
+      public readonly isDeleted: boolean,
       public readonly userId: number,
       public readonly id?: number | null,
       public readonly createAt?: Date | null,
