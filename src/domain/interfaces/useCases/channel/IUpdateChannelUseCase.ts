98654f8b03f5120467eeabc9b0f2e5a0789c361d
Danilo Nobre <danilonobren@gmail.com>
2025-06-01 20:01:49 -0300

    TODO, dúvidas

--- src/domain/interfaces/useCases/channel/IUpdateChannelUseCase.ts
+++ src/domain/interfaces/useCases/channel/IUpdateChannelUseCase.ts
@@ -2,7 +2,10 @@ import { GuildChannel } from "discord.js";
 import { GenericOutputDto } from "../../../dtos/GenericOutputDto";
 import ChannelEntity from "../../../entities/Channel";
 
+// TODO - não deveria usar o DTO aqui?
 export type CreateChannelInput = Omit<ChannelEntity, "id">;
+
+// TODO - precisa usar o DTO aqui?
 export type UpdateChannelInput = ChannelEntity;
 
 export default interface IUpdateChannelUseCase {
