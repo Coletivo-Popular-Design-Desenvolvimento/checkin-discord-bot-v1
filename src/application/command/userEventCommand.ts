1fc4d649ddc746086045cefd83be152cc88c0e4c
Leonardo Cabral <leogweb@gmail.com>
2025-09-10 19:55:48 -0300

    WIP: Casos de uso de eventos de voz

---
+++ src/application/command/userEventCommand.ts
@@ -0,0 +1,86 @@
+import { IUserEventCommand } from "@interfaces/commands/IUserEventCommand";
+import { IDiscordService } from "@services/IDiscordService";
+import {
+  Client,
+  GuildMember,
+  Message,
+  PartialGuildMember,
+  VoiceState,
+} from "discord.js";
+import { ILoggerService } from "@services/ILogger";
+import {
+  CreateUserEventInput,
+  ICreateUserEvent,
+} from "@interfaces/useCases/userEvent/ICreateUserEvent";
+import {
+  LoggerContext,
+  LoggerContextEntity,
+  LoggerContextStatus,
+} from "@type/LoggerContextEnum";
+import { EventType } from "@prisma/client";
+
+export class UserEventCommand implements IUserEventCommand {
+  constructor(
+    private readonly discordService: IDiscordService<
+      Message,
+      GuildMember,
+      PartialGuildMember,
+      Client,
+      VoiceState
+    >,
+    private readonly logger: ILoggerService,
+    private readonly createUserEvent: ICreateUserEvent,
+  ) {
+    this.executeUserJoinOrLeaveAudioEvent();
+  }
+
+  async executeUserJoinOrLeaveAudioEvent(): Promise<void> {
+    this.logger.logToConsole(
+      LoggerContextStatus.SUCCESS,
+      LoggerContext.COMMAND,
+      LoggerContextEntity.USER_EVENT,
+      "UserEventCommand executeUserJoinOrLeaveAudioEvent",
+    );
+    try {
+      this.discordService.onVoiceEventUserChange(async (oldState, newState) => {
+        this.logger.logToConsole(
+          LoggerContextStatus.SUCCESS,
+          LoggerContext.COMMAND,
+          LoggerContextEntity.USER_EVENT,
+          "discordService onVoiceEventUserChange",
+        );
+        const eventType = this.getEventType(oldState, newState);
+        await this.createUserEvent.execute(
+          this.toCreateUserEventInput(newState, eventType),
+        );
+      });
+    } catch (error) {
+      this.logger.logToConsole(
+        LoggerContextStatus.ERROR,
+        LoggerContext.COMMAND,
+        LoggerContextEntity.USER,
+        `executeNewUser | ${error.message}`,
+      );
+    }
+  }
+  private getEventType(
+    oldState: VoiceState | null,
+    newState: VoiceState | null,
+  ): EventType {
+    if (oldState === null && newState !== null) return EventType.JOINED;
+    if (oldState !== null && newState === null) return EventType.LEFT;
+    throw new Error("Unsupported event type");
+  }
+
+  private toCreateUserEventInput(
+    voiceState: VoiceState,
+    eventType: EventType,
+  ): CreateUserEventInput {
+    return {
+      createdAt: new Date(),
+      userPlatformId: voiceState.member.id,
+      channelPlatformId: voiceState.channelId,
+      eventType,
+    };
+  }
+}
--- src/domain/interfaces/services/IDiscordService.ts
+++ src/domain/interfaces/services/IDiscordService.ts
@@ -2,12 +2,14 @@ export interface IDiscordService<
   M = unknown,
   U = unknown,
   P = unknown,
-  T = unknown
+  T = unknown,
+  S = unknown,
 > {
   client: T;
   onDiscordStart(handler: () => void): void;
   onMessage(handler: (message: M) => void): void;
   onNewUser(handler: (member: U) => void): void;
   onUserLeave(handler: (member: U | P) => void): void;
+  onVoiceEventUserChange(handler: (oldState: S, newState: S) => void): void;
   registerEvents(): void;
 }
