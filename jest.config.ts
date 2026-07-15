4cb6b424d8464f901fe1edeb2c341659d1ebcac3
Julio Makdisse Saito <saitodisse@gmail.com>
2026-07-15 17:54:43 -0300

    test: refatora testes do ChannelRepository sem mocks do Prisma

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: JestConfigWithTsJest = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: "<rootDir>/src/",
   }),
+  maxWorkers: 1,
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
 
